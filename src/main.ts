bf3c83cb8effb019f9fe40f162d6ebd7cb7fb10a
Alexander Bonilla <aldaboad@outlook.com>
2022-04-01 11:57:04 -0500

    port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(NeskerModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000).then(data=>{
+    console.log(data)
+  });
 }
 bootstrap();
