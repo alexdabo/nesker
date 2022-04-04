b4a34365d09ace3e7e4ec5e93b99d1095c7c72a3
Alexander Bonilla <aldaboad@outlook.com>
2022-04-04 14:48:09 -0500

    T02 - heroku docker postgres --  Task API

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,8 +1,9 @@
+import { TaskModule } from './task/task.module';
 import { AppModule } from './app/app.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [AppModule],
+  imports: [AppModule, TaskModule],
   controllers: [],
   providers: [],
 })
--- src/nesker.module.ts
+++ src/nesker.module.ts
@@ -1,10 +1,11 @@
+import { DatabaseModule } from './configs/database.module';
 import { StaticModule } from './configs/static.module';
 import { ConfigModule } from '@nestjs/config';
 import { ApiModule } from './api/api.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [ConfigModule.forRoot(), StaticModule, ApiModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, StaticModule, ApiModule],
   controllers: [],
   providers: [],
 })
